430ea11cadd5a32c55a230661e22fe4a8b311b3e
Viktor Tsymbalyuk <viktor.tsymbalyuk@gmail.com>
2020-06-04 20:34:59 +0300

    fix for import

--- init.go
+++ init.go
@@ -7,28 +7,22 @@ import (
 	"os"
 )
 
-var path *string
 var v, vv, vvv *bool
 
 var (
-	DebugLogger *log.Logger
+	DebugLogger   *log.Logger
 	WarningLogger *log.Logger
 	InfoLogger    *log.Logger
 	ErrorLogger   *log.Logger
 )
 
 func init() {
-	path = flag.String("uri", "", "\"uri\" to use")
 	v = flag.Bool("v", false, "verbose level is WARNING")
 	vv = flag.Bool("vv", false, "verbose level is INFO")
 	vvv = flag.Bool("vvv", false, "verbose level is DEBUG")
-	flag.Parse()
-
-	logVerbosity(v, vv, vvv)
-
 }
 
-func logVerbosity(v *bool, vv *bool, vvv *bool) {
+func logVerbosity() {
 	DebugLogger = log.New(ioutil.Discard, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	InfoLogger = log.New(ioutil.Discard, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
 	WarningLogger = log.New(ioutil.Discard, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
@@ -47,5 +41,7 @@ func logVerbosity(v *bool, vv *bool, vvv *bool) {
 	}
 }
 
-
-
+func FlagsParse() {
+	flag.Parse()
+	logVerbosity()
+}
